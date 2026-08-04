1bccb4b603a815d6058ffc21028baef6b3eafde1
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-04 19:16:57 +0200

    Pierwsze kotki za płoty z C++ na linux

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std::string_literals;
+
+int main()
+{
+
+   const auto hello = "Hello World!"s;
+
+
+    std::cout << hello << '\n';
+
+
+    return 0;
+
+}
